11d247f1d19d6815b47d7eb08302905342650cb4
keyboardsurfer <keyboardsurfer@c-base.org>
2013-06-14 11:00:29 +0200

    updates url for pidcat

--- pidcat.rb
+++ pidcat.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Pidcat < Formula
 	homepage 'https://github.com/JakeWharton/pidcat'
-	url 'https://github.com/JakeWharton/pidcat/blob/cf8138a1e95e998b24ad7c4eed538f00a5e77b45/pidcat.py'
-	sha1 'd17fa1d34756ac54248e22e2fa4fcd6a9d22be32'
+	url 'https://raw.github.com/JakeWharton/pidcat/cf8138a1e95e998b24ad7c4eed538f00a5e77b45/pidcat.py'
+	sha1 '34596721bbf150ea3b956732726e6500b08782a8'
 	version "2013-06-14"
 
 	def install
