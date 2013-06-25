deb6cd60be04a9425a5ede44001e86848524f589
keyboardsurfer <keyboardsurfer@c-base.org>
2013-06-25 13:08:33 +0200

    updates pidcat

--- pidcat.rb
+++ pidcat.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class Pidcat < Formula
 	homepage 'https://github.com/JakeWharton/pidcat'
-	url 'https://raw.github.com/JakeWharton/pidcat/571826e8c03b18402025c6d8bd8bad1d367401a8/pidcat.py'
-	sha1 'c6f446a0fcbf1a1c9b488bdc577cc6ed263b3b27'
-	version "571826e8c03b18402025c6d8bd8bad1d367401a8"
+	url 'https://raw.github.com/JakeWharton/pidcat/a99784eaa5f8f6256c46ea224b7e92284fda956c/pidcat.py'
+	sha1 'b1ba04a65ce1b322eb98f62b8970f532d2dc57e6'
+	version 'a99784eaa5f8f6256c46ea224b7e92284fda956c'
 
 	def install
-		File.rename("pidcat.py", "pidcat")
-		bin.install "pidcat"
+		File.rename('pidcat.py', 'pidcat')
+		bin.install 'pidcat'
 	end
 
 end
